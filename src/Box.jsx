d1eb61dee7c911a0a24a57aec81e383c83aa670e
btko <bbttko@gmail.com>
2025-04-19 11:57:31 +0800

    fixed

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,13 +1,23 @@
+import {useState} from 'react';
+
+
 export default function Box() {
+    const [inputValue, setInputValue] = useState('');
+
+
+    function handleChange(event) {
+        setInputValue(event.target.value);
+    }
+
     function log(msg) {
         console.log(msg);
     }
-    const theinput = 'testing';
+
     return (
         <>
-            <textarea name="myInput" />
+            <input name="myInput" value={inputValue} onChange={handleChange}/>
             <button onClick={()=>console.log('haha')}>test</button>
-            <button onClick={()=>log({myInput})}>test2</button>
+            <button onClick={()=>log(inputValue)}>test2</button>
         </>
     );
 }
